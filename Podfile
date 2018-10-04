89886a2e024f30fd6e4b1a0425c80e9936da6e3f
Andrew Gierke <andrew.gierke@gmail.com>
2018-10-03 18:51:42 -0700

    build settings for search headers

--- Podfile
+++ Podfile
@@ -1,16 +1,22 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 
 target 'Legend' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # Comment the next line if you're not using Swift and don't want to use dynamic 
+
+#frameworks
   use_frameworks!
 
   # Pods for Legend
+	pod 'RealmSwift', '~> 3.10'
+	pod 'SwiftyJSON', '~> 4.0'
+	pod 'Nuke', '~> 7.0'
+	pod 'Starscream', '~> 3.0.2'
 
   target 'LegendTests' do
     inherit! :search_paths
     # Pods for testing
-	pod 'RealmSwift'
+	pod 'RealmSwift', '~> 3.10'
 	pod 'SwiftyJSON', '~> 4.0'
 	pod 'Nuke', '~> 7.0'
 	pod 'Starscream', '~> 3.0.2'
--- Pods/Target Support Files/Pods-Legend/Pods-Legend-frameworks.sh
+++ Pods/Target Support Files/Pods-Legend/Pods-Legend-frameworks.sh
@@ -141,6 +141,21 @@ strip_invalid_archs() {
   STRIP_BINARY_RETVAL=1
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Nuke/Nuke.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Nuke/Nuke.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
